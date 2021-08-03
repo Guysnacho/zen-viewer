c0b17fe0c325138e1dbd7b1982cacce070314e56
Samuel <guysnacho2@gmail.com>
2021-08-03 02:09:24 -0500

    Actual equal ui

--- zen-viewer/src/main.js
+++ zen-viewer/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Equal from "equal-vue";
+import "equal-vue/dist/style.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(Equal).mount("#app");
