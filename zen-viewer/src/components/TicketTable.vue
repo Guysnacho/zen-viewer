3c423f664e44a31e7ba542a64ca2f1192717a366
Samuel <guysnacho2@gmail.com>
2021-08-03 19:15:41 -0500

    Passed tickets to table

--- zen-viewer/src/components/TicketTable.vue
+++ zen-viewer/src/components/TicketTable.vue
@@ -1,19 +1,27 @@
 <template>
   <it-collapse>
     <it-collapse-item title="Tickets">
-      <DataTable :value="tickets" :paginator="true" :rows="25"> </DataTable>
+      <DataTable :value="tickets" :paginator="true" :rows="25">
+        <Column field="id" header="ID"></Column>
+        <Column field="id" header="Subject"></Column>
+        <Column field="id" header="Updated at"></Column>
+        <Column field="id" header="Created at"></Column>
+      </DataTable>
     </it-collapse-item>
   </it-collapse>
 </template>
 
 <script>
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 export default {
   name: "TicketTable",
 
-  data(props) {
-    return {
-      tickets: props.tickets,
-    };
+  props: ["tickets"],
+
+  components: {
+    DataTable,
+    Column,
   },
 };
 </script>
